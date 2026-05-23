1d04f82555c91d4901955922b9499deb6a8529b2
emma-2982 <eteye@byupathway.edu>
2026-05-24 03:50:05 +0400

    Add cart footer and total display functionality

--- src/js/cart.js
+++ src/js/cart.js
@@ -31,6 +31,9 @@ function renderCartContents() {
   updateCartCount()
 }
 
+document.querySelector(".cart-footer").classList.remove("hide");
+
+
 function cartItemTemplate(item) {
   const qty = item.quantity || 1
   const lineTotal = item.FinalPrice * qty
