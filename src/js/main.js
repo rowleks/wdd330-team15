233fed81ee7b6cf087f1099dcbfd2921aabcf82e
Rowland Momoh <momohrowland@gmail.com>
2026-05-25 10:02:11 +0100

    feat: dynamically load product categories on homepage
    Implement functionality to fetch and display product categories from a JSON file. Refactor the homepage to use a template for category cards, enhancing maintainability and scalability.

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,26 @@ import { updateCartCount, loadHeaderFooter } from './utils.mjs'
 new Alert()
 updateCartCount()
 loadHeaderFooter()
+loadProductCategories()
+
+async function loadProductCategories() {
+  const res = await fetch('/json/categories.json')
+  const categories = await res.json()
+
+  if (!categories) {
+    return
+  }
+
+  const categoryList = document.querySelector('.category-list')
+  const template = document.getElementById('category-card-template')
+
+  categories.forEach(category => {
+    const clone = template.content.cloneNode(true)
+    const [link, image, title] = clone.querySelectorAll('a, img, h3')
+    link.href = `/product_listing/?category=${category.id}`
+    image.src = `/images/icons/${category.icon}`
+    image.alt = category.name
+    title.textContent = category.name
+    categoryList.appendChild(clone)
+  })
+}
